9906dec35e5453460904230ebbdc1c48fbc4a334
minicarbon <69854241+minicarbon@users.noreply.github.com>
2021-02-21 16:58:04 +0800

    Add files via upload

---
+++ cnn.result.rb
@@ -0,0 +1,14 @@
+$Footmark = "FPGA_Xilinx"
+$Description = "by Vivado"
+
+
+#=== Resource usage ===
+$SLICE = "0"
+$LUT = "5840"
+$FF = "7464"
+$DSP = "65"
+$BRAM ="164"
+$SRL ="149"
+#=== Final timing ===
+$TargetCP = "10.000"
+$CP = "6.927"
